3e15470f6deb1010dcb39e1ff4b87d14083cfcc6
Victor Bergamini <vbergdev@gmail.com>
2022-04-28 09:57:54 -0300

    using bcript

--- src/controllers/user/updateUser.js
+++ src/controllers/user/updateUser.js
@@ -1,6 +1,20 @@
 const User = require("../../models/user");
 
+const bcrypt = require('bcrypt');
+
 const updateUser = async (req, res) => {
+
+  /*
+    if(!findUser) return res.status(404).send();
+    console.log(findUser.password)
+    let senha = 'Info@1234'
+    const resultado = bcrypt.compareSync(senha,findUser.password);
+    console.log(resultado)
+    if(resultado == true) {
+      return res.send(senha)
+    }
+    if(!resultado) return res.send("senha incorreta")
+  */
 
   const fields = Object.keys(req.body);
   const fieldsAllowToUpdate = ["name", "age", "email", "password"]
@@ -16,6 +30,7 @@ const updateUser = async (req, res) => {
       runValidators: true
     });
     if(!updateUser) return res.status(404).send();
+    
     res.send(updateUser)
   } catch (error) {
     res.status(500).send()
